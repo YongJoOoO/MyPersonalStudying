33966710303284a576e9c0901306381eb96e1dd5
YongJOoOo <we072200@naver.com>
2023-04-03 13:49:17 +0900

    Dynamic Programming Algorithm

--- src/to_0403/Main3.java
+++ src/to_0403/Main3.java
@@ -1,10 +1,29 @@
 package to_0403;
 
-public class Main3 {
+import java.util.Scanner;
 
+/* 10-1. 계단 오르기 */
+public class Main3 {
+	static int[] dy;
+	//솔루션
+	public int solution(int n) {
+		//앞의 두 계단 초기화
+		dy[1] = 1;
+		dy[2] = 2;
+		//피보나치처럼 가짓수 확장된다. 
+		for(int i=3; i<=n; i++) {
+			dy[i] = dy[i-2]+dy[i-1];//앞 두계단 합이 현재 계단 
+		}
+		return dy[n];
+	}
+	
+	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Main3 T = new Main3();
+		Scanner kb = new Scanner(System.in);
+		int n= kb.nextInt();
+		dy = new int[n+1];
+		System.out.println(T.solution(n));
 	}
-
 }
--- src/to_0403/Main4.java
+++ src/to_0403/Main4.java
@@ -1,10 +1,27 @@
 package to_0403;
 
-public class Main4 {
+import java.util.Scanner;
 
+/* 10-2. 돌다리 건너기 */
+public class Main4 {
+	static int[] dy;
+	//solution
+	public int solution(int n) {
+		dy[1]=1;
+		dy[2]=2;
+		for(int i=3; i<=n+1; i++) {
+			dy[i]= dy[i-2]+dy[i-1];
+		}
+		return dy[n+1];
+	}
+	
+	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Main4 T = new Main4();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		dy= new int[n+2];
+		System.out.println(T.solution(n));
 	}
-
 }
