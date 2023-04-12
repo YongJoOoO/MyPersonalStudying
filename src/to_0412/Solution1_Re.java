a9c5e31dc9625d69ff467a1d53bdafb39e9a595f
YongJOoOo <we072200@naver.com>
2023-04-12 15:34:11 +0900

    복습 RE

--- src/to_0412/Solution1_Re.java
+++ src/to_0412/Solution1_Re.java
@@ -1,10 +1,32 @@
 package to_0412;
 /* 복습 1-1 . 사다리 타기 */
-public class Solution1_Re {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+class Solution1_Re {
+	//n명의 학생, ladder[][] 에 사다리 정보 들어옴 
+	public char[] solution(int n, int[][] ladder){
+		char[] answer = new char[n];
+		//1) 대문자 알파벳으로 초기화
+		for(int i=0; i<n; i++) {
+			answer[i] = (char) (i+65);
+		}
+		//2) 실질적인 로직 
+		for(int[] line : ladder) {
+			//각 라인별 가로줄 뽑아서 
+			for(int x : line) {
+				//위치 교환
+				char tmp = answer[x];
+				answer[x] = answer[x-1];
+				answer[x-1] = tmp;
+			}
+		}
+		return answer;
 	}
 
+	public static void main(String[] args){
+		Solution1_Re T = new Solution1_Re();
+		System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+		System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+		System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+		System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+	}
 }
--- src/to_0412/Solution2_Re.java
+++ src/to_0412/Solution2_Re.java
@@ -1,10 +1,55 @@
 package to_0412;
 
-public class Solution2_Re {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+/* 1-2. 청소 | RE */
+import java.util.*;
+class Solution2_Re {
+	public int[] solution(int[][] board, int k){
+		//answer 에는 최종 위치 (행, 열) 담아 리턴
+		int[] answer = new int[2]; 
+		int n = board.length;
+		
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, 1, 0, -1};
+		int d = 1, x = 0, y=0, count=0;
+		while(count < k) {
+			count++;
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if(nx <0 || nx >=n || ny<0 || ny >=n || board[nx][ny] == 1) {
+				//회전시키기
+				d = (d+1)%4	;
+				//지속
+				continue;
+			}
+			x = nx;
+			y = ny;
+		}
+		answer[0] = x;
+		answer[1] = y;
+		
+		return answer;
 	}
 
+	public static void main(String[] args){
+		Solution2_Re T = new Solution2_Re();
+		int[][] arr1 = {{0, 0, 0, 0, 0}, 
+			{0, 1, 1, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr1, 10)));
+		int[][] arr2 = {{0, 0, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0, 0}, 
+			{0, 0, 0, 0, 0, 1}, 
+			{1, 1, 0, 0, 1, 0}, 
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr2, 20)));
+		int[][] arr3 = {{0, 0, 1, 0, 0}, 
+			{0, 1, 0, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 0, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr3, 25)));
+	}
 }
