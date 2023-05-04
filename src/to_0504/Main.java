19701055769c1901bf4d84215dbb85d2739f527d
YongJOoOo <we072200@naver.com>
2023-05-04 13:05:14 +0900

    230504

---
+++ src/to_0504/Main.java
@@ -0,0 +1,14 @@
+package to_0504;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(char x : my_string.toCharArray()){
+            if(x != 'a' && x != 'e' && x != 'i' && x != 'o' && x == 'u'){
+                 answer += x;   
+            }
+        }
+        
+        return answer;
+    }
+}
