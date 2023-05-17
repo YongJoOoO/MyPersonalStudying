b166559926a14f39816c17452c6a999181185fc7
YongJOoOo <we072200@naver.com>
2023-05-17 14:14:40 +0900

    230517 공부

--- src/to_0517/Solution.java
+++ src/to_0517/Solution.java
@@ -1,20 +1,26 @@
 package to_0517;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 class Solution {
-    public int solution(String my_string) {
-        int answer = 0;
-        for(char x : my_string.toCharArray()){
-            if(Character.isDigit(x)){
-                answer += Character.getNumericValue(x);
-            }
-        }
+    public long solution(long n) {
+        long answer = 0;
+        
+        double tmp = Math.sqrt(n);
+        
+        if(tmp % 1 == 0) {
+        	//pow( a, b) => a의 b 제곱을 반환한다. 
+        	answer = (long) Math.pow(tmp+1, 2);
+        }else return -1;
+        
         return answer;
     }
     
-    public static void main(String[] args){
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 		Solution T = new Solution();
-		System.out.println(T.solution("aAb1B2cC34oOp"));
+		int n = sc.nextInt();
+		System.out.println(T.solution(n));
 	}
 }
