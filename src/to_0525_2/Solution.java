bf1e23acc50d08458890a6344603296b6f0f70c0
YongJOoOo <we072200@naver.com>
2023-05-25 12:50:47 +0900

    230525

--- src/to_0525_2/Solution.java
+++ src/to_0525_2/Solution.java
@@ -1,11 +1,33 @@
 package to_0525_2;
-/* 4-6. 멀티태스킹 */
+/* 4-6. 멀티태스킹 (어려움) */
 import java.util.*;
 class Solution {
 	//솔루션 함수 
     public int solution(int[] tasks, long k) {
-        	int answer = 0;
-		
+        int answer = 0;
+        int[] sT =  new int[tasks.length +1]; //작업번호 1부터 시작하게
+        System.arraycopy(tasks, 0, sT, 1, tasks.length);
+        //정렬
+        Arrays.sort(sT);
+        int rest = tasks.length;
+        
+        for(int i=1; i<sT.length; i++) {
+        	long time= ((long) rest * (sT[i]-sT[i-1]));
+        	if(k <time) {
+        		long idx = k % rest;
+        		int cnt = 0;
+        		for(int j=0; j<tasks.length; j++) {
+        			if(tasks[j] >= sT[i]) {
+        				if(cnt == idx) return j+1;
+        				cnt++;
+        			}
+        		}
+        	}else {
+        		k -= time;
+        		rest--;
+        	}
+        }
+        
 		return answer;
     }
     //실행 메인 
