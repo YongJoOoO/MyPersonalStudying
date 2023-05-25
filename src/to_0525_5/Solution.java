e3c8014167a1fe46c6ff58435144a98f0f4c7066
YongJOoOo <we072200@naver.com>
2023-05-25 15:23:44 +0900

    스트레스 킹받는다

---
+++ src/to_0525_5/Solution.java
@@ -0,0 +1,15 @@
+package to_0525_5;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        Arrays.sort(num_list);
+        
+        int[] answer = Arrays.copyOfRange(num_list ,5, num_list.length);
+        
+        
+        return answer;
+    }
+}
