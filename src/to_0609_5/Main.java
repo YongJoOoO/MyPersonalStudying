7424b7ed34417777aa86ca42f72023d594ac8a50
YongJOoOo <we072200@naver.com>
2023-06-09 16:28:05 +0900

    230609

---
+++ src/to_0609_5/Main.java
@@ -0,0 +1,15 @@
+package to_0609_5;
+/* 1697번. 숨바꼭질 
+ * */
+public class Main {
+	//시간 체크 배열 
+	static int [] time;
+	
+	//BFS로 풀기 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
