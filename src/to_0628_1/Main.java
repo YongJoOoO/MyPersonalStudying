c96a6eca62a3f3e788603d026d8ab5b04c92f220
YongJOoOo <we072200@naver.com>
2023-06-28 13:58:43 +0900

    union find

--- src/to_0628_1/Main.java
+++ src/to_0628_1/Main.java
@@ -48,26 +48,38 @@ public class Main {
 		
 		party = new ArrayList[M];
 		for(int i=0; i<M; i++) {
-			party[i] = new ArrayList<Integer>();
+			party[i] = new ArrayList<Integer>(); //공간 생성 
+			//파티 사이즈 입력받고 
 			int party_size = kb.nextInt();
 			for(int j=0; j<party_size; j++) {
+				//각 사이즈별 add()처리 
 				party[i].add(kb.nextInt());
 			}
 		}
-		
+		//부모노드 저장용 
 		parent = new int[N+1];
 		for(int i=0; i<=N; i++) {
-			parent[i]=i;
+			parent[i]=i; //자기 자신으로 초기화
 		}
-		
+		//파티 M개의 그룹 만들기 
 		for(int i=0; i<M; i++) {
 			int firstPeople = party[i].get(0);
 			for(int j=1; j<party[i].size(); j++) {
 				union(firstPeople, party[i].get(j));
 			}
 		}
-		
-		
+		//각 파티 대표 노드 vs 진실 대표 노드 같다면 과장 불가능 
+		for(int i=0; i<M; i++) {
+			boolean isPossible = true;
+			int cur = party[i].get(0);
+			for(int j=0; j<trueP.length; j++) {
+				if(find(cur ) == find(trueP[j])) {
+					isPossible = false;
+					break;
+				}
+			}
+			if(isPossible) result++;
+		}		
+		System.out.println(result);
 	}
-
 }
