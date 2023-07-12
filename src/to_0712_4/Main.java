003801c46af718341cdd90dc87ef99ee2df5d8da
YongJOoOo <we072200@naver.com>
2023-07-12 12:38:02 +0900

    동적 계획법 공부

--- src/to_0712_4/Main.java
+++ src/to_0712_4/Main.java
@@ -1,10 +1,25 @@
 package to_0712_4;
-/*백준 */
+
+import java.util.Scanner;
+
+/*백준 12852번. 1로 만들기 2 */
 public class Main {
 	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner kb= new Scanner(System.in);
+		int N = kb.nextInt();
+		
+		int[] D = new int[N+1];
+		D[0] = D[1] = 0;
+		
+		for(int i=2; i<=N; i++) {
+			D[i] = D[i-1] + 1; // 먼저 빼주고 
+			if(i % 2 ==0 ) D[i] = Math.min(D[i], D[i/2] + 1);
+			if(i % 3 == 0) D[i] = Math.min(D[i], D[i/3]+1);
+		}
+		
+		
 	}
 
 }
