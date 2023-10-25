89af5e19268b6e9b786eb537399de7b2159cf57a
YongJOoOo <we072200@naver.com>
2023-10-25 12:24:35 +0900

    231025 오전 백준 DFS, BFS 문제 풀이

---
+++ src/to_1025_1/Main.java
@@ -0,0 +1,81 @@
+package to_1025_1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int L, C;
+    public static char[] list;
+    public static char[] code;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        list = new char[C]; //조합 가능한 문자 개수 
+        code = new char[L]; //4개씩 가능한 암호 배열 
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int x = 0; x < C; x++) {
+            list[x] = st.nextToken().charAt(0); //대상 문자들 입력받고 
+        }
+
+
+        // 정렬
+        Arrays.sort(list);
+
+        makeCode(0,0);
+
+    }
+
+    public static void makeCode(int x,int lv) {
+        if (lv == L) {
+            // 최소 한개의 모음, 최소 2개의 자음인지 확인
+            if (isValid()) {
+                System.out.println(code);
+            }
+            return;
+        }
+
+        // 아직 길이 L의 코드를 만들지 않았고 글자도 아직 남았다.
+
+        for (int i = x; i < C; i++) {
+            code[lv] = list[i];
+            makeCode(i+1, lv + 1);
+        }
+    }
+
+    // 최소 모음 1개, 최소 자음 2개인지 확인
+    public static boolean isValid() {
+        int mo = 0;
+        int ja = 0;
+
+        for (char x : code) {
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+                mo++;
+            } else {
+                ja++;
+            }
+        }
+
+        if (mo >= 1 && ja >= 2) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/to_1025_2/Main.java
@@ -0,0 +1,77 @@
+package to_1025_2;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * 16947번. 서울 지하철 2호선 문풀 
+ * @author MYLG
+ *
+ */
+class Edge implements Comparable<Edge>{
+	int s, e;
+	Edge(int s, int e){
+		this.s = s;
+		this.e = e;
+	}
+	@Override
+	public int compareTo(Edge o) {
+		// TODO Auto-generated method stub
+		return this.s - o.s;
+	}
+}
+public class Main {
+	static int N;
+	static int[] arr;//지하철 개수 
+	static int[] parent;
+	static PriorityQueue<Edge> pQ;
+	
+	//find
+	static int find(int a ) { //사이클 확인용 
+		if(a==parent[a]) return a;
+		else return parent[a] = find(parent[a]);
+	}
+	//uinon
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a!=b) {
+			parent[b]= a;
+		}
+	}
+	
+	//실행 메인 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb= new Scanner(System.in);
+		N = kb.nextInt();
+		arr = new int[N+1];
+		parent= new int[N+1];
+		for(int i=1; i<=N; i++) parent[i] = i;
+		
+		pQ = new PriorityQueue<>();
+		
+		//입력
+		for(int i=0; i<N; i++) {
+			int a = kb.nextInt();
+			int b= kb.nextInt();
+			
+			pQ.offer(new Edge(a, b));
+			
+			union(a, b);
+		}
+		boolean[] isCycle = new boolean[N+1];
+		
+		//판별
+		while(!pQ.isEmpty()) {
+			Edge cur = pQ.poll();
+			if(find(cur.s) == find(cur.e)) {
+				//사이클 존재한 경우 
+				isCycle[cur.s] = true;
+				isCycle[cur.e] = true;
+			}
+		}
+		
+	}
+
+}
