6c704f6ed5ade021acf21ca89dee28c77d2f747f
YongJOoOo <we072200@naver.com>
2024-01-19 21:38:57 +0900

    240119 플로이드에 대한 재풀이

--- src/to_24_0119_1/Main.java
+++ src/to_24_0119_1/Main.java
@@ -1,5 +1,6 @@
-package to_24_0117_1;
+package to_24_0119_1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,7 +10,7 @@ import java.util.Scanner;
  */
 public class Main {
 	static final int INF = 10000001;
-	//실행 메인 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
@@ -19,12 +20,7 @@ public class Main {
 		int[][] distance = new int[V+1][V+1];
 		
 		for(int i=1; i<=V; i++) {
-			for(int j=1; j<=V; j++) {
-				if(i==j) distance[i][j] = 0;
-				else {
-					distance[i][j] = INF;
-				}
-			}
+			Arrays.fill(distance[i], INF); //그냥 모든 distance에 대한 INF 초기화 처리함
 		}
 		
 		//세팅
@@ -56,16 +52,20 @@ public class Main {
 			System.out.println();
 		}
 		
+		/**
+		 * 
+		 * disatnce[i][i] : i -> i : i로 출발해서 i로 도착 (즉, 자기 자신에 대한 경로이므로 0 처리를 해둠)
+		 * 그러나, 채윤님의 경우, i=j 부분을 따로 0처리를 하는 대신 INF로 초기화를 해두었기 때문에, 
+		 * 플로이드 적용 후 distance[i][i] 에 대한 최단거리가 다음과 같이 갱신될 것이다. 
+		 * distance[i][i] = i -> k -> i : 즉, 특정 경유지 k를 거쳐서 들어온 최단 거리를 저장하도록 함 (결과적으로는 사이클이 담김)
+		 *  
+		 */
 		int answer = INF;
-		for(int i=1; i<=V; i++) {
-			for(int j=1; j<=V; j++) {
-				if(i != j && distance[i][j] != INF && distance[j][i] != INF) {
-					answer = Math.min(answer, distance[i][j] + distance[j][i]);
-				}
-			}
+		for(int i=1; i<=V; i++) { 
+			//answer = Math.min(answer, distance[i][j] + distance[j][i] ) 가 아니라 
+			answer = Math.min(answer, distance[i][i]); //이렇게 i,i (즉, i -> 특정 경유지 k -> i 자기 자신으로 되돌아온 (사이클) 최단 경로가 담기게 되는 것이다. )
 		}
-		
-		//if(answer == INF) System.out.println(-1);
-		//else System.out.println(answer);
+		//System.out.println(answer);
 	}
+
 }
